473582a26d93951b622dd6d876473cd607148026
kimromi <kimromi4@gmail.com>
2018-02-14 03:03:52 +0900

    Add configurable module and imap client

--- lib/mailr.rb
+++ lib/mailr.rb
@@ -1,5 +1,19 @@
 require "mailr/version"
+require 'mailr/configurable'
+require "mailr/imap_client"
 
 module Mailr
-  # Your code goes here...
+  class << self
+    def imap_client
+      @imap_client ||= Mailr::ImapClient.new
+    end
+
+    def method_missing(method_name, *args, &block)
+      if imap_client.respond_to?(method_name)
+        return imap_client.send(method_name, *args, &block)
+      end
+
+      super
+    end
+  end
 end
